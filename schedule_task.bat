ddd445c7aa5d426c0fc79e2e815232345df479d9
Matthew Vernacchia <mvernacc@mit.edu>
2020-12-22 22:00:19 -0500

    Use pythonw in scheduled task.

--- schedule_task.bat
+++ schedule_task.bat
@@ -1,1 +1,7 @@
-schtasks /create /tn "Lockscreen GOES Images" /tr "python %~dp0set_lock_screen.py" /sc minute /mo 10
+:: Get the absolute path to the python executable.
+:: Use `pythonw` so it runs without a shell GUI appearing.
+FOR /F "tokens=* USEBACKQ" %%F IN (`where pythonw`) DO (
+SET python_exe=%%F
+)
+:: Schedule a task to periodically run the set_lock_screen.py script.
+schtasks /create /tn "Lockscreen GOES Images" /tr "%python_exe% %~dp0set_lock_screen.py" /sc minute /mo 10
